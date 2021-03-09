239e3ba94b535711bfaa7a68addb9a8f384ec661
leopoldkwok <leopoldkwok@gmail.com>
2021-03-09 23:03:31 +0000

    Fetch Drinks

--- src/context.js
+++ src/context.js
@@ -9,6 +9,44 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
 
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCockTails = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumbnail,
+            strAlcoholic,
+            strGlass,
+          } = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            imgae: strDrinkThumbnail,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCockTails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
+
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
       {children}
