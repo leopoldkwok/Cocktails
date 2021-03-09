73ad7064a4338e693d62587bd24e9c0babc7016c
leopoldkwok <leopoldkwok@gmail.com>
2021-03-09 22:14:11 +0000

    About and Error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
+        praesentium repellat nisi deserunt rem, sequi, atque illum officiis
+        autem soluta iure quasi, nesciunt ex omnis accusantium fugit est? Harum
+        officiis ipsam totam a similique ipsum dicta error, voluptatem fugiat
+        vitae.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
